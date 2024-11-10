ad304a7ccb42080242b7dceb622a9c6a8e0f5518
JeffreyZe <xuze.wang.2017@sis.smu.edu.sg>
2024-11-10 14:26:41 +0800

    6.2 Test 1 for the Date class

--- DemoRisk/TestDate.cpp
+++ DemoRisk/TestDate.cpp
@@ -1,13 +1,53 @@
+#include <iomanip>
+#include <iostream>
+#include <cstdlib>
+#include "Date.h"
+using namespace minirisk;
+
+// Construction of an invalid date should generate an error: generate intentionally 1000 random invalid dates
+// (e.g. 31-Apr-2010) and verify that the Date class constructor throws an error (use try...catch).
 void test1()
 {
-}
+    int num = 0;
+    int total_tests = 1000;
+    int catches = 0;
 
-void test2()
-{
-}
+    srand(static_cast<long>(time(NULL)));
 
-void test3()
-{
+    while (num < total_tests)
+    {
+        num++;
+
+        int y = rand() % 1000 + 1500;
+        int m = rand() % 30 + 13; // invalid months from 13 onwards
+        int d = rand() % 30 + 32; // invalid days from 32 onwards
+
+        try
+        {
+            // Try Date construction, expecting failure
+            Date(y, m, d);
+        }
+        catch (const std::invalid_argument &e)
+        {
+            // Count the exception catches
+            catches++;
+            continue;
+        }
+        catch (...)
+        {
+        }
+
+        std::cout << "Failed to capture invalid date " << d << "-" << m << "-" << y << std::endl;
+    }
+
+    if (catches == total_tests)
+    {
+        std::cout << "Test 1: SUCCESS" << std::endl;
+    }
+    else
+    {
+        throw std::runtime_error("Test 1 failed: Not all invalid dates were caught.");
+    }
 }
 
 int main()
@@ -17,4 +57,3 @@ int main()
     test3();
     return 0;
 }
-
