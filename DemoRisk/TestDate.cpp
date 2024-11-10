6e999f58816366fa64821c3c7c4070f3f260534e
JeffreyZe <xuze.wang.2017@sis.smu.edu.sg>
2024-11-10 14:45:06 +0800

    6.2 Test 2 for the Date class

--- DemoRisk/TestDate.cpp
+++ DemoRisk/TestDate.cpp
@@ -8,15 +8,15 @@ using namespace minirisk;
 // (e.g. 31-Apr-2010) and verify that the Date class constructor throws an error (use try...catch).
 void test1()
 {
-    int num = 0;
+    int count = 0;
     int total_tests = 1000;
-    int catches = 0;
+    int exception_count = 0;
 
     srand(static_cast<long>(time(NULL)));
 
-    while (num < total_tests)
+    while (count < total_tests)
     {
-        num++;
+        count++;
 
         int y = rand() % 1000 + 1500;
         int m = rand() % 30 + 13; // invalid months from 13 onwards
@@ -29,8 +29,8 @@ void test1()
         }
         catch (const std::invalid_argument &e)
         {
-            // Count the exception catches
-            catches++;
+            // Count the exception exception_count
+            exception_count++;
             continue;
         }
         catch (...)
@@ -40,13 +40,55 @@ void test1()
         std::cout << "Failed to capture invalid date " << d << "-" << m << "-" << y << std::endl;
     }
 
-    if (catches == total_tests)
+    if (exception_count == total_tests)
     {
         std::cout << "Test 1: SUCCESS" << std::endl;
     }
     else
     {
         throw std::runtime_error("Test 1 failed: Not all invalid dates were caught.");
+    }
+}
+
+// Verify that converting a date in calendar format (day, month, year) to serial format and then converting back to
+// calendar format yields the original date. Repeat for all dates in the valid range (1-Jan-1900, 31-Dec-2199).
+void test2()
+{
+    int fail_count = 0;
+    const std::array<unsigned, 12> days_in_month = {{31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}};
+
+    // loop through all days from 1-Jan-1900 to 31-Dec-2199
+    for (unsigned y = 1900; y < 2200; ++y)
+    {
+        for (unsigned m = 1; m <= 12; ++m)
+        {
+            unsigned dmax = days_in_month[m - 1] + ((m == 2 && Date::is_leap_year(y)) ? 1 : 0);
+            for (unsigned d = 1; d <= dmax; ++d)
+            {
+
+                // to serial format
+                Date date_obj(y, m, d);
+                // to calendar format
+                std::string date_string = date_obj.to_string();
+
+                // check whether still the same
+                std::string calendar_format = std::to_string(d) + '-' + std::to_string(m) + '-' + std::to_string(y);
+                if (calendar_format != date_string)
+                {
+                    fail_count++;
+                    std::cout << "The conversion for Date " << d << "-" << m << "-" << y << " is failed." << std::endl;
+                }
+            }
+        }
+    }
+
+    if (fail_count == 0)
+    {
+        std::cout << "Test 2: SUCCESS" << std::endl;
+    }
+    else
+    {
+        throw std::runtime_error("Test 2 failed: Calendar and Serial format conversion failed.");
     }
 }
 
